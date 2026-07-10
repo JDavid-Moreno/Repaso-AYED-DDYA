58b16d7477d7a7ce7a0bb4f6dfa2e8a4180ee64e
JDavid-Moreno <davidmorenodaleman@gmail.com>
2026-07-10 00:36:50 -0500

    codigo momentaneo para automatizacion de links de yt

---
+++ automatizar_links.py
@@ -0,0 +1,14 @@
+import re
+
+def front_page(link):
+    match = re.search(r"watch\?v=([A-Za-z0-9_-]{11})", link)
+    return match.group(1) if match else None
+
+def main():
+    link = input("Link: ")
+    name = input("Name: ")
+    front = front_page(link)
+    print(f"[![{name}](https://img.youtube.com/vi/{front}/0.jpg)]({link})")
+
+main()
+ 
